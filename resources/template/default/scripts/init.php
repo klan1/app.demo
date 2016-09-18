1c267461d52ee5a4645acb871c9819a4930b2ecd
Alejandro Trujillo J. <contacto@klan1.com>
2016-09-18 13:53:06 -0500

    Using now Bower and Composer for get all dependencies.

--- resources/template/default/scripts/init.php
+++ resources/template/default/scripts/init.php
@@ -34,20 +34,3 @@ temply::register_footer(APP_RESOURCES_URL . "html5/js/k1app.js");
 temply::set_place_value("html-title", APP_TITLE);
 
 \k1lib\crudlexs\input_helper::$main_css = $main_css;
-
-/**
-<!DOCTYPE html>
-<html>
-<head>
-
-  <title>jQuery Mobile page</title>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/themes/my-custom-theme.css" />
-  <link rel="stylesheet" href="css/themes/jquery.mobile.icons.min.css" />
-  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" /> 
-  <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script> 
-  <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> 
-
-</head>
- */
