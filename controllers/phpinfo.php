0a2771e813bd68fb8c20bd6ce9f066ad67cf61f3
Alejandro Trujillo J. <contacto@klan1.com>
2016-10-12 23:22:13 -0500

    - Improvements

--- controllers/phpinfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace k1app;
-
-//require \k1lib\templates\temply::load_template("header", APP_TEMPLATE_PATH);
-
-phpinfo();
-
-//require \k1lib\templates\temply::load_template("footer", APP_TEMPLATE_PATH);
