13b258230383875b73583c71b2a39d79d5b36348
Alejandro Trujillo J. <contacto@klan1.com>
2016-10-12 22:37:09 -0500

    - Improvements

--- controllers/index.php
+++ controllers/index.php
@@ -2,13 +2,6 @@
 
 namespace k1app;
 
-use k1lib\session\session_db as session_db;
 use k1lib\urlrewrite\url as url;
 
-\k1lib\common\check_on_k1lib();
-
-if (session_db::is_logged()) {
-    \k1lib\html\html_header_go(url::do_url("table-explorer/"));
-} else {
-    \k1lib\html\html_header_go(url::do_url(APP_URL . "log/form/"));
-}
+\k1lib\html\html_header_go(url::do_url("table-explorer/"));
