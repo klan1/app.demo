d587560ef417a875bd1286e2af6f940af343d3e3
Alejandro Trujillo J. <contacto@klan1.com>
2016-10-17 12:48:15 -0500

    - Improvements and some fixes

--- controllers/log/form.php
+++ controllers/log/form.php
@@ -18,23 +18,4 @@ $form_action = \k1lib\urlrewrite\url::do_url("in");
 
 $form_values = \k1lib\common\unserialize_var("login");
 
-switch ($app_error) {
-    case "not-logged":
-        $app_messajes = \k1lib\common\get_error("Debes ingresar primero a la aplicacion");
-        break;
-    case "bad-login":
-        $app_messajes = \k1lib\common\get_error("Usuario y contraseña erroneos");
-        break;
-    case "bad-magic":
-        $app_messajes = \k1lib\common\get_error("Very BAD Magic!!");
-        break;
-    case "no-data":
-        $app_messajes = \k1lib\common\get_error("No se han recibido datos");
-        break;
-    default:
-        $app_messajes = NULL;
-        break;
-}
-
-
 include temply::load_template("html-parts/login", APP_TEMPLATE_PATH);
