94bb5e6573f87b90136e8947eb6a289e5846e76f
Alejandro Trujillo J. <contacto@klan1.com>
2016-10-21 11:44:13 -0500

    - Improvements

--- controllers/log/form.php
+++ controllers/log/form.php
@@ -31,12 +31,12 @@ $form->append_child(new \k1lib\html\input("hidden", "magic_value", $form_magic_v
 $content_grid = new \k1lib\html\foundation\grid_row(1, 1, $form);
 $content_grid->col(1)->medium(6)->medium_centered()->large(4)->large_centered();
 
-$main_grid = $content_grid->col(1)->append_grid(3, 1);
+$main_grid = $content_grid->col(1)->append_grid(4, 1);
 $main_grid->row(1)->col(1)->set_class("text-center")->set_id("k1lib-login-logo")->append_child(new \k1lib\html\img(APP_TEMPLATE_IMAGES_URL . "klan1.png"));
-$main_grid->row(2)->col(1)->set_id("k1lib-login-title")->append_h1(APP_TITLE . " - WELCOME");
+$main_grid->row(2)->col(1)->set_id("k1lib-login-title")->set_class("text-left")->append_h1(APP_TITLE);
+$klan1_link = new \k1lib\html\a("http://www.klan1.com?ref=k1.app", "Klan1 Network", "_blank", NULL, "klan1-site-link");
 
-$login_grid = $main_grid->row(3)->col(1)->append_grid(5, 1)->set_id("k1app-login-content");
-
+$login_grid = $main_grid->row(3)->col(1)->append_grid(6, 1)->set_id("k1app-login-content");
 
 $grid_row_login = (new \k1lib\html\foundation\label_value_row("Login", (new \k1lib\html\input("text", "login", NULL))))->set_attrib("placeholder", "Login")->append_to($login_grid->row(2)->col(1));
 $grid_row_password = (new \k1lib\html\foundation\label_value_row("Password", (new \k1lib\html\input("password", "pass", NULL))))->set_attrib("placeholder", "Password")->append_to($login_grid->row(3)->col(1));
@@ -45,3 +45,7 @@ $grid_row_remember = (new \k1lib\html\foundation\label_value_row("Remember me", 
 $button_grid = $login_grid->row(5)->col(1)->append_row(2);
 $button_grid->col(1)->small(6)->append_a("javascript:alert('Sorry to hear that!, Please contacta an administrator')", "Forgot your password?");
 $button_grid->col(2)->small(6)->set_class("text-right")->append_child(new \k1lib\html\input("submit", NULL, "Login", "button"));
+
+$main_grid->row(4)->col(1)->append_h6("© 2013-2016 Developed by $klan1_link")->set_id("k1lib-login-copyright");
+
+
