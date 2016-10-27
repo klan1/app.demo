f265769d672917cce4f09de79a7df854c6ae082f
Alejandro Trujillo J. <contacto@klan1.com>
2016-10-27 14:07:55 -0500

    Merge

--- controllers/general-utils/select-row-keys.php
+++ controllers/general-utils/select-row-keys.php
@@ -17,10 +17,8 @@ use \k1lib\html\DOM as DOM;
 $body = DOM::html()->body();
 
 include temply::load_template("header", APP_TEMPLATE_PATH);
-if (!isset($_GET['just-controller'])) {
-    include temply::load_template("app-header", APP_TEMPLATE_PATH);
-    include temply::load_template("app-footer", APP_TEMPLATE_PATH);
-}
+include temply::load_template("app-header", APP_TEMPLATE_PATH);
+include temply::load_template("app-footer", APP_TEMPLATE_PATH);
 
 $selector_title = "Select the Key";
 
