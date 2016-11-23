3d389799816eb9fd105a8a50e19adbf3cda924dc
Alejandro Trujillo J. <contacto@klan1.com>
2016-11-23 06:45:19 -0500

    - fixes

--- controllers/app/proveedores.php
+++ controllers/app/proveedores.php
@@ -22,7 +22,7 @@ $controller_name = "Proveedores";
 /**
  * ONE LINE config: less codign, more party time!
  */
-$controller_object = new \k1lib\crudlexs\controller_base(APP_BASE_URL, $db, $db_table_to_use, $controller_name, $top_bar);
+$controller_object = new \k1lib\crudlexs\controller_base(APP_BASE_URL, $db, $db_table_to_use, $controller_name, 'k1lib-title-3');
 $controller_object->set_config_from_class("\k1app\providers_config");
 
 /**
