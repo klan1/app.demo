03f3ed3a1b78463fc3898cd1c1b33339d7589b74
Alejandro Trujillo J. <contacto@klan1.com>
2016-11-23 06:43:54 -0500

    - fixes

--- resources/template/k1phphtml/app-header.php
+++ resources/template/k1phphtml/app-header.php
@@ -20,28 +20,29 @@ if (!isset($_GET['just-controller'])) {
 
     $menu_left = DOM::menu_left();
     $menu_left_tail = DOM::menu_left_tail();
+    if (!empty(DOM::off_canvas())) {
 
-    /**
-     * AUTO APP
-     */
-    if (k1lib_session::is_logged()) {
         /**
-         * APP Preferences
+         * AUTO APP
          */
-        if (\k1lib\session\session_plain::check_user_level(['god'])) {
+        if (k1lib_session::is_logged()) {
+            /**
+             * APP Preferences
+             */
+            if (\k1lib\session\session_plain::check_user_level(['god'])) {
 
+                $admin_menu = $menu_left_tail->add_sub_menu('#', 'App preferences', 'nav-app-preferences');
 
-            $admin_menu = $menu_left_tail->add_sub_menu('#', 'App preferences', 'nav-app-preferences');
+                $admin_menu->add_menu_item(APP_URL . 'table-explorer/show-tables/', 'Table Explorer', 'nav-table-explorer');
+                $admin_menu->add_menu_item(APP_URL . 'table-metadata/show-tables/', 'Manage tables', 'nav-manage-tables');
+                $admin_menu->add_menu_item(APP_URL . 'table-metadata/load-field-comments/', 'Load fields metadata', 'nav-fields-metadata');
+                $admin_menu->add_menu_item(APP_URL . 'table-metadata/export-field-comments/', 'Export field metadata', 'nav-export-fields-meta')->set_attrib('target', '_blank');
+            }
 
-            $admin_menu->add_menu_item(APP_URL . 'table-explorer/show-tables/', 'Table Explorer', 'nav-table-explorer');
-            $admin_menu->add_menu_item(APP_URL . 'table-metadata/show-tables/', 'Manage tables', 'nav-manage-tables');
-            $admin_menu->add_menu_item(APP_URL . 'table-metadata/load-field-comments/', 'Load fields metadata', 'nav-fields-metadata');
-            $admin_menu->add_menu_item(APP_URL . 'table-metadata/export-field-comments/', 'Export field metadata', 'nav-export-fields-meta')->set_attrib('target', '_blank');
+            $menu_left_tail->add_menu_item(url::do_url(APP_URL . 'log/out/'), 'Salir', 'nav-logout');
+        } else {
+            $menu_left_tail->add_menu_item(url::do_url(APP_URL . 'log/form/'), 'Ingresar', 'nav-login');
         }
-
-        $menu_left_tail->add_menu_item(url::do_url(APP_URL . 'log/out/'), 'Salir', 'nav-logout');
-    } else {
-        $menu_left_tail->add_menu_item(url::do_url(APP_URL . 'log/form/'), 'Ingresar', 'nav-login');
     }
 }
 $body->header()->append_div(null, 'k1app-output');
