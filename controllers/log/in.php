0db07bfacf0a935d8cf2b742337eac8837ab2de2
Alejandro Trujillo J. <contacto@klan1.com>
2017-01-28 12:35:13 -0500

    Merge origin/master
    Conflicts:
    	controllers/app/ecards-sends.php
    	controllers/app/ecards.php
    	controllers/app/memberships.php
    	resources/template/k1phphtml/app-header.php

--- controllers/log/in.php
+++ controllers/log/in.php
@@ -20,25 +20,22 @@ if (!isset($app_session)) {
 $app_session->set_config($login_table, $login_user_field, $login_password_field, $login_level_field);
 $app_session->set_inputs($login_user_input, $login_password_input, $login_remember_me);
 
-
 // chekc the magic value
 $post_data = $app_session->catch_post();
 if ($post_data) {
     $app_session_check = $app_session->check_login();
     if ($app_session_check) {
-
-
         $user_data = array_merge($user_data, $app_session_check);
-//        unset($user_data[$login_password_field]);
-// CLEAR ALL
-//        $app_session->unset_coockie(APP_BASE_URL);
+        // unset($user_data[$login_password_field]);
+        // CLEAR ALL
+        // $app_session->unset_coockie(APP_BASE_URL);
         $app_session->end_session();
-// BEGIN ALL AGAIN
+        // BEGIN ALL AGAIN
         $app_session->start_session();
-// SET THE LOGGED SESSION
+        // SET THE LOGGED SESSION
         $app_session->save_data_to_coockie(APP_BASE_URL);
         if ($app_session->load_data_from_coockie($db)) {
-            DOM_notifications::queue_mesasage("Bienvenido", "success");
+            DOM_notifications::queue_mesasage("Wellcome!", "success");
             if (\k1lib\urlrewrite\get_back_url(TRUE)) {
                 \k1lib\html\html_header_go(url::do_url(\k1lib\urlrewrite\get_back_url(TRUE)));
             } else {
@@ -49,9 +46,8 @@ if ($post_data) {
         }
     } elseif ($app_session_check === NULL) {
         DOM_notifications::queue_mesasage("No se han recibido datos", "warning");
-        exit;
-    } elseif ($app_session_check === FALSE) {
-        DOM_notifications::queue_mesasage("Usuario y/o contraseña incorrecta", "alert");
+    } elseif ($app_session_check === array()) {
+        DOM_notifications::queue_mesasage("Bad password or login", "alert");
     }
 } elseif ($post_data === FALSE) {
     DOM_notifications::queue_mesasage("BAD, BAD Magic!!", "warning");
