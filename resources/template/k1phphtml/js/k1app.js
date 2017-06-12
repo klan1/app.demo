94205f39c52fd31b0e231ddd381b33e9ac776f17
Alejandro Trujillo J. <contacto@klan1.com>
2017-06-12 01:27:07 -0500

    - date picker general

--- resources/template/k1phphtml/js/k1app.js
+++ resources/template/k1phphtml/js/k1app.js
@@ -3,6 +3,16 @@ $(document).foundation();
 $(function () {
     // Menu active node opeer
     if (!$('li.active').parent().is($('[data-accordion-menu]'))) {
-        $('[data-accordion-menu]').foundation('toggle', $('li.active').parent());
+        if ($('li.active').parent().size() !== 0) {
+            $('[data-accordion-menu]').foundation('toggle', $('li.active').parent());
+        }
+    }
+    if ($(".datepicker").size() !== 0) {
+        $(".datepicker").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            dateFormat: "yy-mm-dd",
+            showButtonPanel: true,
+        });
     }
 });
