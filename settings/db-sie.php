48ea39d8bbc14a3233aae7003141273102330775
J0hnD03 <alejo@klan1.com>
2019-11-08 04:27:11 -0500

    SOME CHANGES

--- settings/db-sie.php
+++ settings/db-sie.php
@@ -9,9 +9,9 @@ try {
     /**
      * @var \k1lib\db\PDO_k1 
      */
-    $db = new \k1lib\db\PDO_k1("k1app_sie_ch", 'k1appch', 'DBaccess', "klan1.net", "3306", "mysql");
-    $db->set_verbose_level(APP_VERBOSE);
+    $db_sie = new \k1lib\db\PDO_k1("k1app_sie2019", 'sie2017v1', 'DBaccess', "klan1.net", "3306", "mysql");
+    $db_sie->set_verbose_level(APP_VERBOSE);
 } catch (\PDOException $e) {
     trigger_error($e->getMessage(), E_USER_ERROR);
 }
-$db->exec("set names utf8");
+$db_sie->exec("set names utf8");
